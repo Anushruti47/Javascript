e76b2a727be09790d20e36dd736eba8545aa6292
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-12-16 21:52:40 +0530

    updated

---
+++ Chapter1_Output_Print_inJS/console.js
@@ -0,0 +1,13 @@
+console.log(12); //Printing numbers
+console.log("I love Javascript"); //Printing text
+console.log(21+40); //Finding the sum
+console.log(3+4);
+console.log(2+1);
+
+//Printing on the same line
+console.log(3+4,2+1);
+console.log("We need to understand","how to print multiple output on the same line");
+
+//Area and perimeter of the rectangle
+console.log(11*13);
+console.log(2*(11+13));

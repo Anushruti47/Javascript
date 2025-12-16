0cd3b2ff48632c29322bd9350eadaafaa4823caf
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-12-17 00:12:48 +0530

    updated

--- Chapter2_Variables_and_datatypes/variable.js
+++ Chapter2_Variables_and_datatypes/variable.js
@@ -1,2 +1,15 @@
 var age=23;
-console.log(age)
+console.log(age);
+
+//Historical and less preferred
+var age=25;
+console.log(age);
+console.log(age-1);
+
+//Block-scoped
+let name="John Doe";
+console.log(name);
+
+//Block-scoped and constant (cannot be reassigned)
+const PI=3.14;
+console.log(PI);

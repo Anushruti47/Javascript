dd752e2f4606f6d14446ae85d0b71ff63c8ddeb3
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-12-17 01:16:11 +0530

    updated

---
+++ Chapter2_Variables_and_datatypes/2.5_costOfPainting.js
@@ -0,0 +1,5 @@
+var side=14;
+var area=side*side;
+var cost=7*area;
+console.log(area);
+console.log(cost);
---
+++ Chapter2_Variables_and_datatypes/2.6_negative_numbers.js
@@ -0,0 +1,5 @@
+var a=-50, b=40;
+total=a+b;
+product=a*b;
+console.log(total);
+console.log(product);

ed13ca476f87c375a5fb81375969cd435ede70bc
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2025-12-17 00:21:52 +0530

    updated

--- Chapter2_Variables_and_datatypes/variable.js
+++ Chapter2_Variables_and_datatypes/variable.js
@@ -13,3 +13,25 @@ console.log(name);
 //Block-scoped and constant (cannot be reassigned)
 const PI=3.14;
 console.log(PI);
+
+//Properties of variables
+//Reassignment is possible in var and let unlike const
+
+var age=23;
+console.log(age);
+
+let count=10;
+console.log(count);
+
+//Update the age 
+age=24;
+console.log(age);
+
+//Update the count variable by 1
+count=count+1;
+console.log(count);
+
+//Updation not possible in const
+// const PI=3.14;
+
+

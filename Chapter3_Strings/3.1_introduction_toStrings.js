a1d439407bdf86507b088e8e3f3c1305940bc4a1
Anushruti07 <240118848+Anushruti47@users.noreply.github.com>
2026-01-10 23:53:41 +0530

    updated

---
+++ Chapter3_Strings/3.1_introduction_toStrings.js
@@ -0,0 +1,8 @@
+// Creating a string
+var singleQuotedString = 'Hello, World!';
+var doubleQuotedString = "JavaScript is fun!";
+var backticksString = `You can use backticks too!`;
+
+console.log(singleQuotedString);
+console.log(doubleQuotedString);
+console.log(backticksString);
